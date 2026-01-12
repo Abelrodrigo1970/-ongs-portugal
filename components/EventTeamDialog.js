8bf30af6a4e080b1b55656758bc1dd15fd572241
ASO <abelrodrigo101@gmail.com>
2026-01-12 23:05:40 +0000

    Ajustar EventTeamDialog (Dialog 2) conforme Figma: adicionar wrapper dialog-box-content e ajustar estrutura/dimensÃµes

--- components/EventTeamDialog.js
+++ components/EventTeamDialog.js
@@ -207,17 +207,18 @@ const EventTeamDialog = ({ isOpen, onClose, event, onBack }) => {
   return (
     <div className="fixed inset-0 z-[60] flex items-center justify-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
       <div className="dialog-team" onClick={(e) => e.stopPropagation()}>
-        <div className="dialog-box-heading">
-          <button onClick={onBack} className="icon-button">
-            <ArrowLeft className="icon-instance-node" />
-          </button>
-          <div className="close-wrapper">
-            <button onClick={onClose} className="icon-button">
-              <X className="icon-instance-node" />
+        <div className="dialog-box-content">
+          <div className="dialog-box-heading">
+            <button onClick={onBack} className="icon-button">
+              <ArrowLeft className="icon-instance-node" />
             </button>
+            <div className="close-wrapper">
+              <button onClick={onClose} className="icon-button">
+                <X className="icon-instance-node" />
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="div-2">
+          <div className="div-2">
           {/* Left Content */}
           <div className="content">
             <div className="frame-2">
@@ -401,6 +402,7 @@ const EventTeamDialog = ({ isOpen, onClose, event, onBack }) => {
               </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
