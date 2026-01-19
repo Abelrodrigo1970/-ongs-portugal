fb0ddae4eff0b43b3e8d082c063b61157acca76f
ASO <abelrodrigo101@gmail.com>
2026-01-19 11:29:12 +0000

    Atualizar gradiente do header para rgb(141, 170, 251)

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -82,7 +82,7 @@ const Header = () => {
           paddingTop: '32px',
           paddingLeft: '60px',
           paddingRight: '60px',
-          background: 'linear-gradient(90deg, rgb(192, 216, 247), rgb(188, 215, 249))'
+          background: 'linear-gradient(90deg, rgb(141, 170, 251), rgb(141, 170, 251))'
         }}
       >
         <div
